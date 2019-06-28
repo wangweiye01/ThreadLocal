747bc3782a91f56f301d781191f350cb39ce9f06
wangweiye <wwyknight@163.com>
2019-06-28 15:47:22 +0800

    update .gitignore

--- src/TestSync.java
+++ src/TestSync.java
@@ -15,16 +15,27 @@ public class TestSync implements Runnable {
         }
     }
 
+    private void print2() throws InterruptedException {
+        synchronized (this.getClass()) {
+            for (int i = 0; i < 100; i++) {
+                Thread.sleep(100);
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
     @Override
     public void run() {
         try {
-            print();
+            // print();
+            print2();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
+        // example1
         /*Thread thread1 = new Thread(new TestSync(), "name1");
 
         Thread thread2 = new Thread(new TestSync(), "name2");
@@ -32,12 +43,21 @@ public class TestSync implements Runnable {
         thread1.start();
         thread2.start();*/
 
-        TestSync testSync = new TestSync();
+        // example2
+        /*TestSync testSync = new TestSync();
         Thread thread3 = new Thread(testSync, "name1");
 
         Thread thread4 = new Thread(testSync, "name2");
 
         thread3.start();
-        thread4.start();
+        thread4.start();*/
+
+        // example3
+        Thread thread1 = new Thread(new TestSync(), "name1");
+
+        Thread thread2 = new Thread(new TestSync(), "name2");
+
+        thread1.start();
+        thread2.start();
     }
 }
